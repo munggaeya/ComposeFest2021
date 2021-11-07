77860e03a9af0f77052628a01c2f15c052803628
stace <munggaeya@gmail.com>
2021-11-07 22:36:45 +0900

    1주차 11/14

--- week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/example/mybasicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/example/mybasicscodelab/MainActivity.kt
@@ -3,16 +3,18 @@ package com.example.mybasicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.layout.relocationRequester
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mybasicscodelab.ui.theme.MyBasicsCodelabTheme
@@ -30,9 +32,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp(names : List<String> = listOf("World", "Compose")) {
-    Column (modifier = Modifier.padding(vertical = 4.dp)){
-        for(name in names){
+private fun MyApp() {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if(shouldShowOnboarding){
+        OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
+    }else {
+        Greetings()
+    }
+}
+
+@Composable
+private fun Greetings(names : List<String> = List(1000){"$it"}){
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names){ name ->
             Greeting(name = name)
         }
     }
@@ -40,21 +53,61 @@ private fun MyApp(names : List<String> = listOf("World", "Compose")) {
 
 @Composable
 private fun Greeting(name: String) {
+
+    val expanded = remember { mutableStateOf(false) }
+
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
-            OutlinedButton(
-                onClick = { /*TODO*/ }
-            ) {
-                Text(text = "Show more")
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
+    }
+}
+
+
+@Composable
+fun OnboardingScreen(onContinueClicked : () -> Unit){
+
+    Surface{
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ){
+                Text(text = "Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true , widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+    MyBasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
 
@@ -65,4 +118,3 @@ private fun DefaultPreview() {
         MyApp()
     }
 }
-
